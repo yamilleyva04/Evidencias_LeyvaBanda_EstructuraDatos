be7df9068db5c1b7851cc240903e8e1dd9508945
yamilleyva04 <leyvabandaangelyamil@gmail.com>
2025-09-25 13:39:06 -0700

    Add files via upload
    Par e Impar en python, Leyva Banda Angel Yamil

---
+++ ParImpar.py
@@ -0,0 +1,29 @@
+def main():
+    numeros = []
+    par = 0
+    imp = 0
+    suma = 0
+
+    print("Da 10 números:")
+
+    for i in range(10):
+        num = int(input(f"Número {i+1}: "))
+        numeros.append(num)
+        suma += num
+
+        if num % 2 == 0:
+            par += 1
+        else:
+            imp += 1
+
+    prom = suma / 10
+
+    print("\n--- Resultados ---")
+    print("Números:", numeros)
+    print("Pares:", par)
+    print("Impares:", imp)
+    print("Promedio:", prom)
+
+
+if __name__ == "__main__":
+    main()
